85ba569d5ec976e5523632cf2b529e5bc7dd57b2
Ravinder Singla <rsingla511@gmail.com>
2022-07-07 00:00:21 +0530

    navbar design completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.callButton}>
+
+            <Image src='/img/telephone.png' width='32px' height='32px' alt='' />
+          </div>
+            <div className={styles.texts}>
+                <div className={styles.text}>ORDER NOW!</div>
+                <div className={styles.text}>628 016 2039</div>
+            </div>
+        </div>
+        <div className={styles.item}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>Home</li>
+            <li className={styles.listItem}>Products</li>
+            <li className={styles.listItem}>Menu</li>
+            <Image src="/img/logo.png" width='160px' height='69px' />
+            <li className={styles.listItem}>Events</li>
+            <li className={styles.listItem}>Blog</li>
+            <li className={styles.listItem}>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" width='30px' height='30px' />
+            <div className={styles.counter}>2</div>
+            
+          </div>
+        </div>
+
+    </div>
   )
 }
 
